0b3606fa0e3e5ca5f97db8a47dff9c29c1e647d2
GamaX320 <bryan.x320@gmail.com>
2015-12-26 14:01:50 +0800

    Add volley library and able to insert record to web hosting

--- app/src/main/java/com/tarpost/bryanty/proj_t_post/activity/AddInformationActivity.java
+++ app/src/main/java/com/tarpost/bryanty/proj_t_post/activity/AddInformationActivity.java
@@ -1,16 +1,63 @@
 package com.tarpost.bryanty.proj_t_post.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.tarpost.bryanty.proj_t_post.R;
+import com.tarpost.bryanty.proj_t_post.application.MyApplication;
+import com.tarpost.bryanty.proj_t_post.json.JSONParser;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AddInformationActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+    private String title,content;
+    private EditText etInformationTitle, etInformationContent;
+    private FloatingActionButton btSubmit;
+
+    private TextView textView;
+
+    private ProgressDialog pdProgressAdd;
+
+    private RequestQueue requestQueue;
+
+    //http://localhost/tarpost/addInformation.php
+    private static final String ADD_INFORMATION_URL = "http://projx320.webege.com/tarpost/php/insertPostWithoutImage.php";
+
+    //http://projx320.byethost4.com/tarpost/addInformation.php
+
+    //JSON element ids from repsonse of php script:
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +69,21 @@ public class AddInformationActivity extends ActionBarActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //initial input fields
+        etInformationTitle = (EditText)findViewById(R.id.informationTitle);
+        etInformationContent= (EditText)findViewById(R.id.informationContent);
+
+        title = etInformationTitle.getText().toString();
+        content = etInformationContent.getText().toString();
+
+        //initial button
+        btSubmit = (FloatingActionButton)findViewById(R.id.button_add_information);
+
+//        //request queue
+//        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        pdProgressAdd = new ProgressDialog(this);
+        pdProgressAdd.setMessage("Adding...");
+        pdProgressAdd.setCancelable(false);
 
     }
 
@@ -45,5 +107,46 @@ public class AddInformationActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addInformation(View v) {
+
+        //Showing progress dialog
+        pdProgressAdd.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ADD_INFORMATION_URL
+                , new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                pdProgressAdd.dismiss();
+                etInformationTitle.setText("");
+                etInformationContent.setText("");
+                Toast.makeText(getApplicationContext(), "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),""+title+" - "+content , Toast.LENGTH_SHORT)
+                        .show();
+
+                Log.d("response" ,"Register Response: " + response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                pdProgressAdd.dismiss();
+                Toast.makeText(getApplicationContext(), "Failed to insert", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("creatorId", "1");
+                params.put("title", "asd");
+                params.put("content", "asd");
+
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        MyApplication.getInstance().addToReqQueue(stringRequest);
     }
 }
---
+++ app/src/main/java/com/tarpost/bryanty/proj_t_post/application/MyApplication.java
@@ -0,0 +1,50 @@
+package com.tarpost.bryanty.proj_t_post.application;
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by BRYANTY on 12-Dec-2015.
+ */
+public class MyApplication extends Application {
+
+    private RequestQueue mRequestQueue;
+    private static MyApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized MyApplication getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getReqQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToReqQueue(Request<T> req, String tag) {
+
+        getReqQueue().add(req);
+    }
+
+    public <T> void addToReqQueue(Request<T> req) {
+
+        getReqQueue().add(req);
+    }
+
+    public void cancelPendingReq(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
